26a4674fc95c7c6226e9a84e017bf51e558040d8
SheylaAlv <sheyla_alvarezr@hotmail.com>
2020-03-11 15:46:55 -0700

    first attempt get trend posts

--- code/server/index.js
+++ code/server/index.js
@@ -1,4 +1,5 @@
 import {create_user} from './create_user.js';
+import {get_trending_posts} from './get_trending_posts.js';
 
 const express = require('express');
 const cors = require('cors');
@@ -15,7 +16,11 @@ app.post('/create_user', (req, res) => {
     console.log(req.body);
     create_user(req.body);
 });
-
+app.post('/get_trending_posts', (req, res) => {
+    console.log(req.body);
+    console.log(get_trending_posts());
+    // res.send(get_trending_posts());
+});
 app.listen(3000, () => {
     console.log(process.env.PORT);
 });
